fa092ce46f576f6ab2d7d81ae3c6ca2ecfc70851
Marcelo Simim <54868944+marcelosimim@users.noreply.github.com>
2022-03-03 14:02:20 -0300

    Add files via upload

---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDoList
+//
+//  Created by Marcelo Simim Santos on 03/03/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  CoreDataToDoList
+//
+//  Created by Marcelo Simim Santos on 03/03/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
